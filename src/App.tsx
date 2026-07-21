2770bd1c39ea78c778abfe76927f463d47a8181f
Eyinade Peter <itemize6@gmail.com>
2026-07-21 11:28:00 +0100

    added some landing components

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Signup from "./pages/Signup";
 import SignIn from "./pages/SignIn";
 import ForgotPassword from "./pages/ForgotPassword";
 import CreateNewPassword from "./pages/CreateNewPassword";
+import Home from "./pages/Home";
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 			<Route path="/signin" element={<SignIn />} />
 			<Route path="/forgot-password" element={<ForgotPassword />} />
 			<Route path="/create-new-password" element={<CreateNewPassword />} />
+			<Route path="/home" element={<Home />} />
 		</Routes>
 	);
 }
